ea266ee505980b733776bb07facc055107fcb400
Jango Soriano <nere183s@gmail.com>
2026-09-24 14:21:55 -0700

    Added a placeholder section in home/page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,21 +2,28 @@ import styles from "./home.module.css";
 
 export default function home() {
   return (
-    <nav className="navbar">
-      <h1 className="logo">
-        <a href="index.html">Placeholder</a>
-      </h1>
-      <ul className="nav-list">
-        <li>
-          <a href="/home">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <main>
+      <nav className="navbar">
+        <h1 className="logo">
+          <a href="index.html">Placeholder</a>
+        </h1>
+        <ul className="nav-list">
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <section
+        id="Placeholder section ID"
+        className="Placeholder className"
+      ></section>
+    </main>
   );
 }
