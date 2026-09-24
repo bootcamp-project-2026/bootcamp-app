79515422c22a83171e6c9d5fe91d34dc2b26b5dc
Jango Soriano <nere183s@gmail.com>
2026-09-24 14:04:06 -0700

    Created contact, home, and about folders

---
+++ src/app/home/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./home.module.css";
+
+export default function about() {
+  return <div></div>;
+}
---
+++ src/contact/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./contact.module.css";
+
+export default function about() {
+  return <div></div>;
+}
