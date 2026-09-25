a4f52ebf5848420105b8357711ae7c6ae53bf94e
Jango Soriano <nere183s@gmail.com>
2026-09-24 17:37:23 -0700

    Moved navbar from home/page.tsx to components/Navbar.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,23 +3,6 @@ import styles from "./home.module.css";
 export default function home() {
   return (
     <main>
-      <nav className="navbar">
-        <h1 className="logo">
-          <a href="index.html">Placeholder</a>
-        </h1>
-        <ul className="nav-list">
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-
       <section
         id="Placeholder section ID"
         className="Placeholder className"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,20 @@
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <h1 className="logo">
+        <a href="index.html">Placeholder</a>
+      </h1>
+      <ul className="nav-list">
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
